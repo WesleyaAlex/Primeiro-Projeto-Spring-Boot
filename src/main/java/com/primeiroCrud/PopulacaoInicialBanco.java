78105f5105377dcba0f2a8c1fed5742c2de15bad
unknown <wesley132013@gmail.com>
2021-09-11 19:14:46 -0300

    Criação do CRUD

---
+++ src/main/java/com/primeiroCrud/PopulacaoInicialBanco.java
@@ -0,0 +1,37 @@
+package com.primeiroCrud;
+
+import java.time.LocalDate;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.primeiroCrud.rh.dominio.Pessoa;
+import com.primeiroCrud.rh.dominio.PessoaRepositorio;
+
+@Component
+@Transactional
+public class PopulacaoInicialBanco implements CommandLineRunner {
+
+	@Autowired
+	private PessoaRepositorio pessoaRepo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Pessoa p1 = new Pessoa("Joao");
+		p1.setDataNascimento(LocalDate.of(1990, 4, 1));
+		p1.setEmail("joao@gmail.com");
+		p1.setCpf("33333333333");
+		p1.setTelefone("11 987878779");
+		Pessoa p2 = new Pessoa("Maria");
+		p2.setDataNascimento(LocalDate.of(2090, 4, 1));
+		p2.setEmail("maria@gmail.com");
+		p2.setCpf("44444444444");
+		p2.setTelefone("11 909891890");
+		
+		pessoaRepo.save(p1);
+		pessoaRepo.save(p2);
+	}
+}
---
+++ src/test/java/com/primeiroCrud/PrimeiroCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeiroCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
